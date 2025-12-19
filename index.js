2e996a2855e37a64ba7a111e202957d6529ac3aa
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-12-19 20:10:36 +0500

    NodeIntro:get request

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express";
 
 const app = express()
 
+app.get("/",(req,res)=>{
+    res.send("Hello Node")
+})
+
 
 app.listen(3000,()=>{
     console.log("your server is running on 3000")
