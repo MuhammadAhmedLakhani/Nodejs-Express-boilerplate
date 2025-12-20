a12bf303db1fc3f521218cc57cdcc823965c88be
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-12-20 14:19:04 +0500

    Postman

--- index.js
+++ index.js
@@ -3,17 +3,47 @@ import express from "express";
 
 const app = express()
 
+console.log("running app")
+
+
 app.get("/",(req,res)=>{
     res.send("Hello Node")
 })
 app.get("/users",(req,res)=>{
     res.send({
-        name:"Ahmed",
+        name:"Muhammad Ahmed raza  ",
         email:"ahmed@gmail.com"
     })
 })
 
 
+app.post("/posts",(req,res )=>{
+    res.send({message:"Post Request received"})
+})
+
+
+
+
 app.listen(3000,()=>{
     console.log("your server is running on 3000")
 })
+
+
+
+    // --post request 
+
+
+// fetch("http://localhost:3000/posts", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json" // Crucial for servers to read the body
+//     },
+//     body: JSON.stringify({ name: "Ahmed" }) // Ahmed must be in quotes
+// })
+// .then((res) => res.json()) // json() must have parentheses ()
+// .then((res1) => {
+//     console.log(res1); // Success handler
+// })
+// .catch((error) => {
+//     console.log("Error detected:", error); // Error handler
+// });
